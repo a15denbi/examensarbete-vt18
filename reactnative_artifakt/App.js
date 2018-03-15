a6f25c67cadcaea2ad79c21d9e54f4d0308cfe4e
Dennis B <a15denbi@student.his.se>
2018-03-15 23:59:49 +0100

    added support for AlbumList component
    Added support for AlbumList component + general clean up

--- reactnative_artifakt/App.js
+++ reactnative_artifakt/App.js
@@ -5,22 +5,12 @@
 //  Created by Dennis Bilberg on 2018-03-15.
 //  Copyright © 2018 Dennis Bilberg. All rights reserved.
 //
-
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Text } from 'react-native';
 import Header from './src/Header';
-import Deck from './src/Deck';
+import AlbumList from './src/AlbumList';
 
-// exempel require lokal img
-/* <Image source={require('./my-icon.png')} />
-
-<Image source={require('./assets/greece1.jpeg')} />
-<Image source={require('./assets/london1.jpeg')} />
-<Image source={require('./assets/paris1.jpg')} />
-*/
-
-
-// fixa url/uri lokal länk!
+// The dataArrayList
 const DATA = [
   { id: 1, text: 'Santorini #1', img: require('./assets/greece1.jpeg') },
   { id: 2, text: 'London #2', img: require('./assets/london1.jpeg') },
@@ -28,20 +18,12 @@ const DATA = [
 ];
 
 export default class App extends React.Component {
-  renderCard(item) {
-    return (
-      <Text>{item.text}</Text>
-    );
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-        <Header />
-        <Deck
-          data={DATA}
-          renderCard={this.renderCard}
-        />
+      //style={styles.container} i view
+      <View>
+        <Header headerText={'Travel Buddy'} />   
+        <AlbumList />     
       </View>
     );
   }
