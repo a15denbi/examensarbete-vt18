0b70c3b3e36a1203eb71607b0b1f3b7a43fbbeef
Dennis B <a15denbi@student.his.se>
2018-03-18 20:46:58 +0100

    App.js updated, added flatlist data rendering
    Added flatlist data rendering

--- reactnative_artifakt/App.js
+++ reactnative_artifakt/App.js
@@ -6,24 +6,42 @@
 //  Copyright © 2018 Dennis Bilberg. All rights reserved.
 //
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 import Header from './src/Header';
-import AlbumList from './src/AlbumList';
+import SelectText from './src/SelectText';
+import ListItem from './src/ListItem';
 
 // The dataArrayList
-const DATA = [
-  { id: 1, text: 'Santorini #1', img: require('./assets/greece1.jpeg') },
-  { id: 2, text: 'London #2', img: require('./assets/london1.jpeg') },
-  { id: 3, text: 'Paris #3', img: require('./assets/paris1.jpg') },
-];
+// const cards = [
+//   { id: "0", title: "Santorini", picture: require('./assets/greece1.jpeg'), content: <Text>Greece pic</Text> },
+//   { id: "1", title: "London", picture: require('./assets/london1.jpeg'), content: <Text>Big Ben</Text> },
+//   { id: "2", title: "Paris", picture: require('./assets/paris1.jpg'), content: <Text>Mona Lisa</Text> },  
+// ];
 
+// the lifecycle
 export default class App extends React.Component {
   render() {
     return (
       //style={styles.container} i view
       <View>
-        <Header headerText={'Travel Buddy'} />   
-        <AlbumList />     
+        <Header headerText={'Travel Buddy'} />  
+        <SelectText />
+        <FlatList 
+          data={[
+            require('./assets/greece1.jpeg'),
+            require('./assets/london1.jpeg'),
+            require('./assets/paris1.jpg'),
+          ]}
+          renderItem={({ item }) => {
+            return <ListItem image={item} />
+          }}
+          keyExtractor={
+            (index) => {
+              return index
+            }
+          }
+        />
       </View>
     );
   }
@@ -32,8 +50,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
 });
